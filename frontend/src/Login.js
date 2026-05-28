156eb1c9759e8b4a2e52c613b84cd381c9669772
Sandra17-lab <sandra17lunaz@gmail.com_GITHUB>
2026-05-27 18:33:35 -0600

    hardcode railway url

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import axios from "axios";
 
-const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5000";
+const API_URL = "https://swapi-mern-production.up.railway.app";
 
 function Login({ onLogin, onGoToRegister }) {
   const [email, setEmail] = useState("");
