41d0410a47c9a8e74d86f6bef667ee954050102c
Sandra17-lab <sandra17lunaz@gmail.com_GITHUB>
2026-05-28 10:50:16 -0600

    hardcode api url in config.js

--- frontend/src/Register.js
+++ frontend/src/Register.js
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import axios from "axios";
-
-const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5000";
+import API_URL from "./config";
 
 function Register({ onLogin, onGoToLogin }) {
   const [username, setUsername] = useState("");
---
+++ frontend/src/config.js
@@ -0,0 +1,3 @@
+const API_URL = "https://swapi-mern-production.up.railway.app";
+
+export default API_URL;
